34c0962d05a8754a2a6e3dd84f9b8be7a0e851cf
michael_soldatkin <michael.soldatkin@gmail.com>
2024-08-19 12:25:18 +0300

    kafka consumer service_provider

--- internal/app/service_provider.go
+++ internal/app/service_provider.go
@@ -7,29 +7,27 @@ import (
 
 	"github.com/IBM/sarama"
 	redigo "github.com/gomodule/redigo/redis"
+	"github.com/mikhailsoldatkin/auth/internal/api/user"
 	"github.com/mikhailsoldatkin/auth/internal/client/kafka"
-	"github.com/mikhailsoldatkin/platform_common/pkg/cache"
-	"github.com/mikhailsoldatkin/platform_common/pkg/cache/redis"
-	"github.com/mikhailsoldatkin/platform_common/pkg/db"
-	"github.com/mikhailsoldatkin/platform_common/pkg/db/pg"
-	"github.com/mikhailsoldatkin/platform_common/pkg/db/transaction"
-
+	kafkaConsumer "github.com/mikhailsoldatkin/auth/internal/client/kafka/consumer"
+	"github.com/mikhailsoldatkin/auth/internal/config"
+	"github.com/mikhailsoldatkin/auth/internal/repository"
 	logRepository "github.com/mikhailsoldatkin/auth/internal/repository/log"
 	pgRepository "github.com/mikhailsoldatkin/auth/internal/repository/user/pg"
 	redisRepository "github.com/mikhailsoldatkin/auth/internal/repository/user/redis"
 	"github.com/mikhailsoldatkin/auth/internal/service"
-	userService "github.com/mikhailsoldatkin/auth/internal/service/user"
-
-	"github.com/mikhailsoldatkin/platform_common/pkg/closer"
-
-	"github.com/mikhailsoldatkin/auth/internal/api/user"
-	"github.com/mikhailsoldatkin/auth/internal/config"
-	"github.com/mikhailsoldatkin/auth/internal/repository"
-
-	kafkaConsumer "github.com/mikhailsoldatkin/auth/internal/client/kafka/consumer"
 	userSaverConsumer "github.com/mikhailsoldatkin/auth/internal/service/consumer/user_saver"
+	userService "github.com/mikhailsoldatkin/auth/internal/service/user"
+	"github.com/mikhailsoldatkin/platform_common/pkg/cache"
+	"github.com/mikhailsoldatkin/platform_common/pkg/cache/redis"
+	"github.com/mikhailsoldatkin/platform_common/pkg/closer"
+	"github.com/mikhailsoldatkin/platform_common/pkg/db"
+	"github.com/mikhailsoldatkin/platform_common/pkg/db/pg"
+	"github.com/mikhailsoldatkin/platform_common/pkg/db/transaction"
 )
 
+// serviceProvider provides access to various services and dependencies required by the application.
+// It manages database connections, Redis clients, Kafka consumers, and service instances.
 type serviceProvider struct {
 	config *config.Config
 
@@ -53,10 +51,13 @@ type serviceProvider struct {
 	userImplementation *user.Implementation
 }
 
+// newServiceProvider creates and returns a new instance of serviceProvider.
 func newServiceProvider() *serviceProvider {
 	return &serviceProvider{}
 }
 
+// Config returns the configuration used by the serviceProvider.
+// If the config is not loaded, it loads the configuration from the source.
 func (s *serviceProvider) Config() *config.Config {
 	if s.config == nil {
 		cfg, err := config.Load()
@@ -69,6 +70,8 @@ func (s *serviceProvider) Config() *config.Config {
 	return s.config
 }
 
+// DBClient returns the database client used by the serviceProvider.
+// If the client is not initialized, it creates a new database client and performs a health check.
 func (s *serviceProvider) DBClient(ctx context.Context) db.Client {
 	if s.dbClient == nil {
 		cl, err := pg.New(ctx, s.Config().DB.PostgresDSN)
@@ -88,6 +91,8 @@ func (s *serviceProvider) DBClient(ctx context.Context) db.Client {
 	return s.dbClient
 }
 
+// TxManager returns the transaction manager used by the serviceProvider.
+// If the transaction manager is not initialized, it creates a new transaction manager.
 func (s *serviceProvider) TxManager(ctx context.Context) db.TxManager {
 	if s.txManager == nil {
 		s.txManager = transaction.NewTransactionManager(s.DBClient(ctx).DB())
@@ -96,6 +101,8 @@ func (s *serviceProvider) TxManager(ctx context.Context) db.TxManager {
 	return s.txManager
 }
 
+// PGRepository returns the PostgreSQL repository used by the serviceProvider.
+// If the repository is not initialized, it creates a new PostgreSQL repository.
 func (s *serviceProvider) PGRepository(ctx context.Context) repository.UserRepository {
 	if s.pgRepository == nil {
 		s.pgRepository = pgRepository.NewRepository(s.DBClient(ctx))
@@ -104,6 +111,8 @@ func (s *serviceProvider) PGRepository(ctx context.Context) repository.UserRepos
 	return s.pgRepository
 }
 
+// RedisPool returns the Redis connection pool used by the serviceProvider.
+// If the pool is not initialized, it creates a new Redis connection pool with the configured settings.
 func (s *serviceProvider) RedisPool() *redigo.Pool {
 	if s.redisPool == nil {
 		s.redisPool = &redigo.Pool{
@@ -120,6 +129,8 @@ func (s *serviceProvider) RedisPool() *redigo.Pool {
 	return s.redisPool
 }
 
+// RedisClient returns the Redis client used by the serviceProvider.
+// If the client is not initialized, it creates a new Redis client and performs a health check.
 func (s *serviceProvider) RedisClient(ctx context.Context) cache.RedisClient {
 	if s.redisClient == nil {
 		cl := redis.NewClient(s.RedisPool(), redis.Config(s.config.Redis))
@@ -134,6 +145,8 @@ func (s *serviceProvider) RedisClient(ctx context.Context) cache.RedisClient {
 	return s.redisClient
 }
 
+// RedisRepository returns the Redis repository used by the serviceProvider.
+// If the repository is not initialized, it creates a new Redis repository.
 func (s *serviceProvider) RedisRepository(ctx context.Context) repository.UserRepository {
 	if s.redisRepository == nil {
 		s.redisRepository = redisRepository.NewRepository(s.RedisClient(ctx))
@@ -142,6 +155,8 @@ func (s *serviceProvider) RedisRepository(ctx context.Context) repository.UserRe
 	return s.redisRepository
 }
 
+// LogRepository returns the log repository used by the serviceProvider.
+// If the repository is not initialized, it creates a new log repository.
 func (s *serviceProvider) LogRepository(ctx context.Context) repository.LogRepository {
 	if s.logRepository == nil {
 		s.logRepository = logRepository.NewRepository(s.DBClient(ctx))
@@ -150,6 +165,8 @@ func (s *serviceProvider) LogRepository(ctx context.Context) repository.LogRepos
 	return s.logRepository
 }
 
+// UserSaverConsumer returns the user saver consumer service used by the serviceProvider.
+// If the service is not initialized, it creates a new user saver consumer service.
 func (s *serviceProvider) UserSaverConsumer(ctx context.Context) service.ConsumerService {
 	if s.userSaverConsumer == nil {
 		s.userSaverConsumer = userSaverConsumer.NewService(
@@ -161,6 +178,8 @@ func (s *serviceProvider) UserSaverConsumer(ctx context.Context) service.Consume
 	return s.userSaverConsumer
 }
 
+// Consumer returns the Kafka consumer used by the serviceProvider. If the consumer is not initialized,
+// it creates a new Kafka consumer and registers it for cleanup.
 func (s *serviceProvider) Consumer() kafka.Consumer {
 	if s.consumer == nil {
 		s.consumer = kafkaConsumer.NewConsumer(
@@ -173,6 +192,8 @@ func (s *serviceProvider) Consumer() kafka.Consumer {
 	return s.consumer
 }
 
+// ConsumerGroup returns the Kafka consumer group used by the serviceProvider. If the consumer group is not initialized,
+// it creates a new Kafka consumer group with the configured settings.
 func (s *serviceProvider) ConsumerGroup() sarama.ConsumerGroup {
 	if s.consumerGroup == nil {
 		consumerGroup, err := sarama.NewConsumerGroup(
@@ -190,6 +211,8 @@ func (s *serviceProvider) ConsumerGroup() sarama.ConsumerGroup {
 	return s.consumerGroup
 }
 
+// ConsumerGroupHandler returns the Kafka consumer group handler used by the serviceProvider.
+// If the handler is not initialized, it creates a new Kafka consumer group handler.
 func (s *serviceProvider) ConsumerGroupHandler() *kafkaConsumer.GroupHandler {
 	if s.consumerGroupHandler == nil {
 		s.consumerGroupHandler = kafkaConsumer.NewGroupHandler()
@@ -198,6 +221,8 @@ func (s *serviceProvider) ConsumerGroupHandler() *kafkaConsumer.GroupHandler {
 	return s.consumerGroupHandler
 }
 
+// UserService returns the user service used by the serviceProvider.
+// If the user service is not initialized, it creates a new user service.
 func (s *serviceProvider) UserService(ctx context.Context) service.UserService {
 	if s.userService == nil {
 		s.userService = userService.NewService(
@@ -211,6 +236,8 @@ func (s *serviceProvider) UserService(ctx context.Context) service.UserService {
 	return s.userService
 }
 
+// UserImplementation returns the user implementation used by the serviceProvider.
+// If the implementation is not initialized, it creates a new user implementation.
 func (s *serviceProvider) UserImplementation(ctx context.Context) *user.Implementation {
 	if s.userImplementation == nil {
 		s.userImplementation = user.NewImplementation(s.UserService(ctx))
