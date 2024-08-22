1257bc7f3c8b646b15c5b61d211856d44b10e76f
michael_soldatkin <michael.soldatkin@gmail.com>
2024-08-22 15:15:34 +0300

    auth TLS

--- internal/app/app.go
+++ internal/app/app.go
@@ -16,6 +16,7 @@ import (
 	"github.com/rakyll/statik/fs"
 	"github.com/rs/cors"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/credentials/insecure"
 	"google.golang.org/grpc/reflection"
 
@@ -164,8 +165,14 @@ func (a *App) initServiceProvider(_ context.Context) error {
 
 // initGRPCServer initializes the GRPC server.
 func (a *App) initGRPCServer(ctx context.Context) error {
+
+	creds, err := credentials.NewServerTLSFromFile("cert/service.pem", "cert/service.key")
+	if err != nil {
+		log.Fatalf("failed to load TLS keys: %v", err)
+	}
+
 	a.grpcServer = grpc.NewServer(
-		grpc.Creds(insecure.NewCredentials()),
+		grpc.Creds(creds),
 		grpc.UnaryInterceptor(interceptor.ValidateInterceptor),
 	)
 
